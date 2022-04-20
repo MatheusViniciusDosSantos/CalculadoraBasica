1ecbf78e3ee953ac96385be37ebb3ecf6e69e742
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-04-19 21:48:39 -0300

    Feito método de definir o tipo de operação a ser realizada

--- calculadora_basica/lib/calculoAoContrario.dart
+++ calculadora_basica/lib/calculoAoContrario.dart
@@ -1,4 +1,29 @@
 import 'dart:math';
+import 'dart:io';
+
+void show() {
+  int controlador = 0;
+  print('''
+  Escolha uma opção de tipo de cálculo.
+    1 - Realizar operação inversa a escolhida
+''');
+  controlador = int.parse(stdin.readLineSync()!);
+  if (controlador == 1) {}
+}
+
+int definirTipoOperacao() {
+  print('''
+  Escolha a operação:
+    1 - Adição.
+    2 - Subtração.
+    3 - Multiplicação.
+    4 - Divisão.
+    5 - Potencia quadrática.
+    6 - Raiz quadrada.
+''');
+
+  return int.parse(stdin.readLineSync()!);
+}
 
 double somaAoContrario(double valor1, double valor2) {
   return valor1 - valor2;
