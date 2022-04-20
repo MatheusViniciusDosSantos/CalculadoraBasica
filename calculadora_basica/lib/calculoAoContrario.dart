aaf53afab3f64752618ebdeb646abd7e7c8857b7
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-04-19 22:42:08 -0300

    Molhorias da função e acrescimo de informações

--- calculadora_basica/lib/calculoAoContrario.dart
+++ calculadora_basica/lib/calculoAoContrario.dart
@@ -3,12 +3,33 @@ import 'dart:io';
 
 void show() {
   int controlador = 0;
+  int operacao = 0;
+  List<double> valores = [];
   print('''
   Escolha uma opção de tipo de cálculo.
     1 - Realizar operação inversa a escolhida
 ''');
   controlador = int.parse(stdin.readLineSync()!);
-  if (controlador == 1) {}
+  if (controlador == 1) {
+    operacao = definirTipoOperacao();
+    
+    if (operacao == 1) {
+      valores = definirValores();
+      somaAoContrario(valores[0], valores[1]);
+    } else if (operacao == 2) {
+      valores = definirValores();
+      subtracaoAoContrario(valores[0], valores[1]);
+    } else if (operacao == 3) {
+      valores = definirValores();
+      multiplicacaoAoContrario(valores[0], valores[1]);
+    } else if (operacao == 4) {
+      valores = definirValores();
+      divisaoAoContrario(valores[0], valores[1]);
+    } else if (operacao == 5) {
+      //Terminar
+      aoQuadradoAoContrario(valores[0]);
+    }
+  }
 }
 
 int definirTipoOperacao() {
@@ -20,9 +41,20 @@ int definirTipoOperacao() {
     4 - Divisão.
     5 - Potencia quadrática.
     6 - Raiz quadrada.
-''');
+  ''');
 
   return int.parse(stdin.readLineSync()!);
+}
+
+List<double> definirValores() {
+  print("Digite o primeiro valor da operação: ");
+  double valor1 = double.parse(stdin.readLineSync()!);
+  print("Digite o segundo valor da operação: ");
+  double valor2 = double.parse(stdin.readLineSync()!);
+  List<double> valores = [];
+  valores.add(valor1);
+  valores.add(valor2);
+  return valores;
 }
 
 double somaAoContrario(double valor1, double valor2) {
