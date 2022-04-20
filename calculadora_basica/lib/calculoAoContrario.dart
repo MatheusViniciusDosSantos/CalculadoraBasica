7b916c01ac352f56ccb14e694335e68d2b2417f8
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-04-19 21:21:19 -0300

    Iniciando calculadora

---
+++ calculadora_basica/lib/calculoAoContrario.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+double somaAoContrario(double valor1, double valor2) {
+  return valor1 - valor2;
+}
+
+double subtracaoAoContrario(double valor1, double valor2) {
+  return valor1 + valor2;
+}
+
+double multiplicacaoAoContrario(double valor1, double valor2) {
+  return valor1 / valor2;
+}
+
+double divisaoAoContrario(double valor1, double valor2) {
+  return valor1 * valor2;
+}
+
+double raizQuadradaAoContrario(double valor1) {
+  return valor1 * valor1;
+}
+
+double aoQuadradoAoContrario(double valor1) {
+  return sqrt(valor1);
+}
